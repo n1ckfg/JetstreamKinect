db874275f6194ea7ebfd5fe9f2a390852447027e
Nick Fox-Gieg <nick@fox-gieg.com>
2020-12-28 18:22:07 -0500

    first

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# https://gist.github.com/madelinegannon/c212dbf24fc42c1f36776342754d81bc
+
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo apt-add-repository https://packages.microsoft.com/ubuntu/18.04/multiarch/prod
+sudo apt-get update
+
+sudo apt install k4a-tools
+sudo apt install libk4a1.4-dev
+
+git clone https://github.com/n1ckfg/Azure-Kinect-Sensor-SDK ../
+cd ../Azure-Kinect-Sensor-SDK
+sudo cp scripts/99-k4a.rules /etc/udev/rules.d/
