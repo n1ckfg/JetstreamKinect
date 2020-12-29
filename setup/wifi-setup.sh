6ac1d4918921316f282fa5ba22eaee4bfa74bce9
Nick Fox-Gieg <nick@fox-gieg.com>
2020-12-28 23:57:21 -0500

    shell

--- setup/wifi-setup.sh
+++ setup/wifi-setup.sh
@@ -1,13 +1,7 @@
 # https://www.datatobiz.com/2019/10/03/fixing-wifi-connectivity-nvidia-jetson-nano/
+# https://github.com/pvaret/rtl8192cu-fixes
 
-sudo apt-get install git linux-headers-generic build-essential dkms
-
-cd ../..
-git clone https://github.com/pvaret/rtl8192cu-fixes
-sudo dkms add ./rtl8192cu-fixes
-sudo dkms install 8192cu/1.11
-sudo depmod -a
-sudo cp ./rtl8192cu-fixes/blacklist-native-rtl8192.conf /etc/modprobe.d/
+sudo cp blacklist-native-rtl8192.conf /etc/modprobe.d/
 sudo echo options rtl8xxxu ht40_2g=1 dma_aggregation=1 | sudo tee /etc/modprobe.d/rtl8xxxu.conf
 sudo iw dev wlan0 set power_save off
 
