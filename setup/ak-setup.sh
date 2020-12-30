89c388e9e1ecae7a94b72f29b1b01f62123257e5
n1ckfg <nick@fox-gieg.com>
2020-12-30 02:16:42 -0500

    shell

--- setup/ak-setup.sh
+++ setup/ak-setup.sh
@@ -4,7 +4,11 @@ curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo apt-add-repository https://packages.microsoft.com/ubuntu/18.04/multiarch/prod
 sudo apt-get update
 
-sudo apt install k4a-tools
-sudo apt install libk4a1.4-dev
+# body tracking SDK requires an older version of the base SDK and must go first
+sudo apt-get install libk4abt1.0 libk4abt1.0-dev
+
+# k4a-tools and the newer base sdk have a dependency conflict with the body tracking sdk 
+#sudo apt-get install libk4a1.4 libk4a1.4-dev 
+#sudo apt-get install k4a-tools 
 
 sudo cp 99-k4a.rules /etc/udev/rules.d/
