1ccf6c5817d3a37e4cc478b19d4381b995553bc8
Nick Fox-Gieg <nick@fox-gieg.com>
2020-12-30 02:53:01 -0500

    shell

--- setup/ak-setup.sh
+++ setup/ak-setup.sh
@@ -1,14 +1,18 @@
 # https://gist.github.com/madelinegannon/c212dbf24fc42c1f36776342754d81bc
 
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo apt-add-repository https://packages.microsoft.com/ubuntu/18.04/multiarch/prod
-sudo apt-get update
+if [ ${MACHINE_TYPE} == 'aarch64' ]; then
+  curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+  sudo apt-add-repository https://packages.microsoft.com/ubuntu/18.04/multiarch/prod
+  sudo apt-get update
 
-# body tracking SDK requires an older version of the base SDK and must go first
-sudo apt-get install libk4abt1.0 libk4abt1.0-dev
+  sudo apt-get install libk4a1.4 libk4a1.4-dev k4a-tools
+else
+  # body tracking SDK requires an older version of the base SDK and must go first
+  sudo apt-get install libk4abt1.0 libk4abt1.0-dev
 
-# k4a-tools and the newer base sdk have a dependency conflict with the body tracking sdk 
-#sudo apt-get install libk4a1.4 libk4a1.4-dev 
-#sudo apt-get install k4a-tools 
+  # k4a-tools and the newer base sdk have a dependency conflict with the body tracking sdk 
+  #sudo apt-get install libk4a1.4 libk4a1.4-dev k4a-tools
+fi
 
 sudo cp 99-k4a.rules /etc/udev/rules.d/
+
--- setup/sdk-build.sh
+++ setup/sdk-build.sh
@@ -1,5 +1,9 @@
-# linux64
-# sudo apt-get install uuid-dev libsoundio-dev xorg-dev
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'aarch64' ]; then
+  
+else
+  sudo apt-get install uuid-dev libsoundio-dev xorg-dev
+fi
 
 mkdir build
 cd build
